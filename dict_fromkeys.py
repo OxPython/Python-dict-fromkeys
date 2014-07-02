54ee270a881095c414d97047eef2a0480aea430e
Emerson <viejoemer@gmail.com>
2014-07-02 14:24:41 -0500

    How to use dict.fromkeys in Python?

---
+++ dict_fromkeys.py
@@ -0,0 +1,23 @@
+'''
+Created on Jul 2, 2014
+
+@author: viejoemer
+
+How to use dict.fromkeys in Python?
+
+¿Cómo utilizar dict.fromkeys en Python?
+
+The method fromkeys() creates a new dictionary with 
+keys from seq and values set to value.
+'''
+
+#This is the tuple of values which would be used for dictionary keys preparation.
+seq = ("one", "two", "three")
+
+#Using fromkeys function from dict (keys)
+d = dict.fromkeys(seq)
+print(d)
+
+#Using fromkeys function from dict (keys, values)
+d = dict.fromkeys(seq,10)
+print(d)
